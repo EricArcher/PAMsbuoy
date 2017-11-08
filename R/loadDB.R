6a9ca5e3c8925207329b440aab3df5d221ab69fe
TaikiSan21 <taiki.sakai@noaa.gov>
2017-11-08 14:27:02 -0800

    Only fail on critical tables, for effort and annotations just make an empty placeholder with a warning

--- R/loadDB.R
+++ R/loadDB.R
@@ -33,12 +33,26 @@ loadDB <- function(fname, verbose = FALSE) {
   attr(db, "station") <- basename(fname)
 
   # check for necessary tables
-  tbls <- c("DIFAR_Localisation", "Listening_Effort", "HydrophoneStreamers",
-            "Spectrogram_Annotation", "gpsData")
-  if(!all(tbls %in% names(db))) {
+  criticalTables <- c("DIFAR_Localisation", "HydrophoneStreamers", "gpsData")
+  if(!all(criticalTables %in% names(db))) {
     stop(paste("Some necessary tables are missing in database ", fname,'. \n',
-               'Missing tables are: ', paste(tbls[!(tbls %in% names(db))], collapse=' ')))
+               'Missing tables are: ', paste(criticalTables[!(criticalTables %in% names(db))], collapse=' ')))
   }
+  # These aren't critical to all the setup, just used for effort on/off
+  usefulTables <- c('Listening_Effort', 'Spectrogram_Annotation')
+  if(!all(usefulTables %in% names(db))) {
+    missing <- usefulTables[!(usefulTables %in% names(db))]
+    message(paste('Some useful tables are missing in database ', fname, '. \n',
+                  'Missing tables are: ', paste(missing, collapse=' ')))
+    # If they are missing just create empty DFs with all the columns we might look up later so it doesnt die
+    for(m in missing) {
+      neededCols <-c('Status', 'Channels', 'Note', 'UTC', 'Duration')
+      db[[m]] <- data.frame(matrix(ncol=length(neededCols), nrow=0))
+      colnames(db[[m]]) <- neededCols
+    }
+  }
+
+
 
   db$Listening_Effort$Status <- tolower(str_trim(db$Listening_Effort$Status))
   db$DIFAR_Localisation$Species <- tolower(str_trim(db$DIFAR_Localisation$Species))
