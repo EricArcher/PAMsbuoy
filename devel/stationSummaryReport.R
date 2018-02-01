26a2f70ae8da376506057230630f226976fdfe59
Taiki Sakai <tnsakai@gmail.com>
2018-01-31 16:45:23 -0800

    adding style document for word output

--- devel/stationSummaryReport.R
+++ devel/stationSummaryReport.R
@@ -5,8 +5,9 @@ library(kableExtra)
 library(ggplot2)
 library(PAMsbuoy)
 library(webshot)
-stationSummaryReport <- function(stationList, title='Sonobuoy Deployment Summary', outdir='Report', format='word') {
-  reportDirs <- paste0(outdir, c('', '/Tables', '/Figures'))
+stationSummaryReport <- function(stationList, title='Sonobuoy Deployment Summary',
+                                 fileName='reportTemplate', outDir='Report', format='word') {
+  reportDirs <- paste0(outDir, c('', '/Tables', '/Figures'))
   for(dir in reportDirs[which(!dir.exists(reportDirs))]) {
     dir.create(dir)
   }
@@ -17,51 +18,51 @@ stationSummaryReport <- function(stationList, title='Sonobuoy Deployment Summary
   detectionPlotCombined <- mapDetections(detSummary, map=myMap, value='NumDetections', size=2)
   detectionPlot <- mapDetections(detSummary, combine=FALSE, map=myMap, value='NumDetections', size=2)
 
-  unlink(list.files(paste0(outdir, '/Figures'), full.names=TRUE))
+  unlink(list.files(paste0(outDir, '/Figures'), full.names=TRUE))
 
-  ggsave(filename='stationPlotTitle.jpeg', plot=stationPlotTitle, path=paste0(outdir, '/Figures'),
+  ggsave(filename='stationPlotTitle.jpeg', plot=stationPlotTitle, path=paste0(outDir, '/Figures'),
          width=4, height=3, units='in', scale=2)
-  ggsave(filename='stationPlot.jpeg', plot=stationPlot, path=paste0(outdir, '/Figures'),
+  ggsave(filename='stationPlot.jpeg', plot=stationPlot, path=paste0(outDir, '/Figures'),
          width=4, height=3, units='in', scale=2)
-  ggsave(filename='detectionPlotCombined.jpeg', plot=detectionPlotCombined, path=paste0(outdir, '/Figures'),
+  ggsave(filename='detectionPlotCombined.jpeg', plot=detectionPlotCombined, path=paste0(outDir, '/Figures'),
          width=4*.8, height=3*.8, units='in', scale=2)
-  ggsave(filename='detectionPlot.jpeg', plot=detectionPlot, path=paste0(outdir, '/Figures'),
+  ggsave(filename='detectionPlot.jpeg', plot=detectionPlot, path=paste0(outDir, '/Figures'),
          width=8*.8, height=4*.8, units='in', scale=1.5)
 
   tempRows <- 10
   detSummary <- head(detSummary, tempRows)
 
-  unlink(list.files(paste0(outdir, '/Tables'), full.names=TRUE))
+  unlink(list.files(paste0(outDir, '/Tables'), full.names=TRUE))
 
   htmlTableToImage(makeHtmlTable(detSummary), tableRows = nrow(detSummary),
-                   outdir=paste0(outdir, '/Tables'), filename = 'detectionSummaryTable',maxRows = 5)
+                   outDir=paste0(outDir, '/Tables'), fileName = 'detectionSummaryTable',maxRows = 5)
 
   switch(format,
          html = {
-           outFile <- 'reportTemplate.html'
+           outFile <- paste0(fileName, '.html')
            outFormat <- 'html_document'
          },
          word = {
-           outFile <- 'reportTemplate.docx'
+           outFile <- paste0(fileName, '.docx')
            outFormat <- 'word_document'
            })
   rmarkdown::render(input='reportTemplate.Rmd', output_file = outFile,
-                    output_dir = outdir,quiet=TRUE, output_format=outFormat)
+                    output_dir = outDir,quiet=TRUE, output_format=outFormat)
 }
 
 htmlTableToImage <- function(inTable, tableRows, headerHeight=59, rowHeight=37,
-                             outdir='Report', filename='table',  maxRows=30) {
+                             outDir='Report', fileName='table',  maxRows=30) {
   # Height 59 if no cap, 94 if cap
   # Off by a pixel
   if(is.null(webshot:::find_phantom())) {
     webshot::install_phantomjs()
   }
-  outPath <- paste0(outdir, '/')
+  outPath <- paste0(outDir, '/')
   tmp <- tempfile('tmpTable', fileext = '.html')
   myTable <- inTable
   rmarkdown::render('tableTemplate.Rmd', tmp, output_format='html_document', quiet=TRUE)
   for(tbls in 1:ceiling(tableRows/maxRows)) {
-    imageName <- paste0(outPath, filename, '_', tbls, '.png')
+    imageName <- paste0(outPath, fileName, '_', tbls, '.png')
     if(tbls==1) {
       top <- 0
       length <- min(maxRows, tableRows)*rowHeight + headerHeight
